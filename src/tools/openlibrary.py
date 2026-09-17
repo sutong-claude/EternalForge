8f95dbcf94a5daf1e214b98d834d24b846e54678
sutong-claude <sutong985211666@gmail.com>
2026-09-17 10:23:11 -0400

    Include DoajAdapter in MultiAdapter fan-out

--- src/tools/openlibrary.py
+++ src/tools/openlibrary.py
@@ -10,6 +10,7 @@ from urllib.request import Request, urlopen
 from tools.arxiv import ArxivAdapter
 from tools.crossref import CrossrefAdapter
 from tools.datacite import DataCiteAdapter
+from tools.doaj import DoajAdapter
 from tools.europepmc import EuropePMCAdapter
 from tools.hackernews import HackerNewsAdapter
 from tools.openalex import OpenAlexAdapter
@@ -170,6 +171,7 @@ class MultiAdapter:
             OpenAlexAdapter(timeout=timeout),
             ZenodoAdapter(timeout=timeout),
             DataCiteAdapter(timeout=timeout),
+            DoajAdapter(timeout=timeout),
         ]
 
     def search(self, query: str, max_results: int = 5) -> list[Hit]:
