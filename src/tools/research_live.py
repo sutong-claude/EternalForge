6686fc99323a3256a0fb716633a3e04726ce9adf
sutong-claude <sutong985211666@gmail.com>
2026-09-23 02:32:50 -0400

    feat: register oaauthorpos in live dispatch

--- src/tools/research_live.py
+++ src/tools/research_live.py
@@ -140,6 +140,7 @@ _SPECS: list[tuple[tuple[str, ...], str, str]] = [
     (("oaauthordname", "authordname-oa", "works-authordname-oa"), "oaauthordname", "OaAuthorDnameAdapter"),
     (("oaauthorcorr", "authorcorr-oa", "works-authorcorr-oa"), "oaauthorcorr", "OaAuthorCorrAdapter"),
     (("oaauthoraffil", "authoraffil-oa", "works-authoraffil-oa"), "oaauthoraffil", "OaAuthorAffilAdapter"),
+    (("oaauthorpos", "authorpos-oa", "works-authorpos-oa"), "oaauthorpos", "OaAuthorPosAdapter"),
 ]
 
 _LIVE = {alias: (mod, cls) for aliases, mod, cls in _SPECS for alias in aliases}
