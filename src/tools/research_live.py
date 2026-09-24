f5993faffdfa703dd6dfec3bddd8e07e78be4433
sutong-claude <sutong985211666@gmail.com>
2026-09-23 21:14:29 -0400

    Register oaaward aliases in live SearchAdapter factory

--- src/tools/research_live.py
+++ src/tools/research_live.py
@@ -156,6 +156,7 @@ _SPECS: list[tuple[tuple[str, ...], str, str]] = [
     (("oasdgs", "sdgs-oa", "works-sdgs-oa"), "oasdgs", "OaSdgsAdapter"),
     (("oagrants", "grants-oa", "works-grants-oa"), "oagrants", "OaGrantsAdapter"),
     (("oatopicid", "topicid-oa", "works-topicid-oa"), "oatopicid", "OaTopicIdAdapter"),
+    (("oaaward", "award-oa", "works-award-oa"), "oaaward", "OaAwardAdapter"),
 ]
 
 _LIVE = {alias: (mod, cls) for aliases, mod, cls in _SPECS for alias in aliases}
