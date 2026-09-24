f277e1a1d7f7030e1b50fdc1727cc9f66fb06960
sutong-claude <sutong985211666@gmail.com>
2026-09-24 04:07:31 -0400

    Register oaconcid aliases in live SearchAdapter dispatch.

--- src/tools/research_live.py
+++ src/tools/research_live.py
@@ -163,6 +163,7 @@ _SPECS: list[tuple[tuple[str, ...], str, str]] = [
     (("oaprtopic", "prtopic-oa", "works-prtopic-oa"), "oaprtopic", "OaPrTopicAdapter"),
     (("oakw", "keywords-oa", "works-keywords-oa"), "oakw", "OaKwAdapter"),
     (("oakwid", "kwid-oa", "works-kwid-oa"), "oakwid", "OaKwIdAdapter"),
+    (("oaconcid", "concid-oa", "works-concid-oa"), "oaconcid", "OaConcIdAdapter"),
 ]
 
 _LIVE = {alias: (mod, cls) for aliases, mod, cls in _SPECS for alias in aliases}
